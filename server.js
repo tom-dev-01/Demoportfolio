7bd457056caf89eb304ab1e3a7a327b20571db13
tom-dev-01 <your-email@gmail.com>
2026-08-23 14:36:11 +0300

    Added frontend HTML page

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
+app.use(express.static('public'));
 
 console.log('🚀 Starting server...');
 console.log('📡 Environment:', {
